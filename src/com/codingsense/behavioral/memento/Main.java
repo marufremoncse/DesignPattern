f37a12b3a9fc5bb07b954a0f5a233104d6bc80fd
marufremoncse <marufremon.cse14.2@gmail.com>
2024-03-19 23:38:40 +0600

    memento pattern 2

--- src/com/codingsense/behavioral/memento/Main.java
+++ src/com/codingsense/behavioral/memento/Main.java
@@ -15,11 +15,14 @@ public class Main {
 
 		originator.setState("State #4");
 		System.out.println("Current State: " + originator.getState());
+		careTaker.add(originator.saveStateToMemento());
 
 		originator.getStateFromMemento(careTaker.get(0));
 		System.out.println("First saved State: " + originator.getState());
 		originator.getStateFromMemento(careTaker.get(1));
 		System.out.println("Second saved State: " + originator.getState());
+		originator.getStateFromMemento(careTaker.get(2));
+		System.out.println("Third saved State: " + originator.getState());
 	}
 
 }
